80a952efa6bca109c453d953480f1cd5f1d609ee
Butsan Vitaliy <skolkojvastut@gmail.com>
2019-11-15 16:38:24 +0200

    Add files

---
+++ BoysAndGirls/ViewControllers/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  BoysAndGirls
+//
+//  Created by Vitaliy on 15.11.2019.
+//  Copyright © 2019 Vitaliy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.backgroundColor = .lightGray
+    }
+}
+
