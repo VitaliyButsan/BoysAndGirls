7125870d2209e1b0a652506c6f13ef6b74938101
Butsan Vitaliy <skolkojvastut@gmail.com>
2019-11-15 16:35:30 +0200

    Add networking manager

---
+++ BoysAndGirls/DataModel/DataModel.swift
@@ -0,0 +1,30 @@
+//
+//  DataModel.swift
+//  BoysAndGirls
+//
+//  Created by Vitaliy on 15.11.2019.
+//  Copyright © 2019 Vitaliy. All rights reserved.
+//
+
+import Foundation
+
+struct PhotoDataWrapper: Decodable {
+    let total: Int
+    let results: [UnsplashPhoto]
+}
+
+struct UnsplashPhoto: Decodable {
+    let width: Int
+    let height: Int
+    let urls: [URLKind.RawValue: String]
+    
+    enum URLKind: String {
+        case raw
+        case full
+        case regular
+        case small
+        case thumb 
+    }
+}
+
+
---
+++ BoysAndGirls/Networking/NetworkManager.swift
@@ -0,0 +1,57 @@
+//
+//  NetworkManager.swift
+//  BoysAndGirls
+//
+//  Created by Vitaliy on 15.11.2019.
+//  Copyright © 2019 Vitaliy. All rights reserved.
+//
+
+import Foundation
+
+class NetworkManager {
+    
+    static let instance = NetworkManager()
+    
+    private init() { }
+    
+    func request(searchingWord: String, callBack: @escaping(Data?, Error?) -> Void) {
+        let parameters = self.prepareParameters(searchWord: searchingWord)
+        let url = self.url(params: parameters)
+        var request = URLRequest(url: url)
+        request.allHTTPHeaderFields = self.prepareHeaders()
+        request.httpMethod = "GET"
+        let task = self.createDataTask(from: request, callBack: callBack)
+        task.resume()
+    }
+    
+    private func prepareParameters(searchWord: String?) -> [String:String] {
+        var parameters = [String:String]()
+        parameters["query"] = searchWord
+        parameters["page"] = String(1)
+        parameters["per_page"] = String(30)
+        return parameters
+    }
+    
+    private func url(params: [String:String]) -> URL {
+        var components = URLComponents()
+        components.scheme = "https"
+        components.host = "api.unsplash.com"
+        components.path = "/search/photos"
+        components.queryItems = params.map { URLQueryItem(name: $0, value: $1) }
+        return components.url!
+    }
+    
+    private func prepareHeaders() -> [String:String] {
+        var headers = [String:String]()
+        headers["Authorization"] = "Client-ID 9854adf3c8c24f42c23b2fc3855c2bbfe82eed43fd79387a3cdbc441eb3537c4"
+        return headers
+    }
+    
+    private func createDataTask(from request: URLRequest, callBack: @escaping (Data?, Error?) -> Void) -> URLSessionDataTask {
+        return URLSession.shared.dataTask(with: request) { (data, _, error) in
+            DispatchQueue.main.async {
+                callBack(data, error)
+            }
+        }
+    }
+}
