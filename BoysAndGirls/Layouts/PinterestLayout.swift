2de58dfdc9968594602801f8ac9f772760c67410
Butsan Vitaliy <skolkojvastut@gmail.com>
2019-12-15 21:55:17 +0200

    Adding headerView

--- BoysAndGirls/Layouts/PinterestLayout.swift
+++ BoysAndGirls/Layouts/PinterestLayout.swift
@@ -12,7 +12,7 @@ protocol PinterestLayoutDelegate: class {
     func collectionView(_ collectionView: UICollectionView, heightForPhotoAtIndexPath indexPath: IndexPath) -> CGFloat
 }
 
-class PinterestLayout: UICollectionViewLayout {
+class PinterestLayout: UICollectionViewFlowLayout {
 
     weak var delegate: PinterestLayoutDelegate?
     
@@ -32,6 +32,7 @@ class PinterestLayout: UICollectionViewLayout {
     }
     
     override func prepare() {
+
         guard cache.isEmpty, let collectionView = collectionView else { return }
         
         let columnWidth = contentWidth / CGFloat(numberOfColumns)
@@ -44,8 +45,14 @@ class PinterestLayout: UICollectionViewLayout {
         var yOffset: [CGFloat] = .init(repeating: 0, count: numberOfColumns)
         
         for item in 0..<collectionView.numberOfItems(inSection: 0) {
-            let indexPath = IndexPath(item: item, section: 0)
             
+            // offset for header
+            if item == 0 || item == 1 {
+                yOffset[column] = yOffset[column] + 30
+            }
+            
+            let indexPath = IndexPath(item: item, section: 0)
+
             let photoHeight = delegate?.collectionView(collectionView, heightForPhotoAtIndexPath: indexPath) ?? 100
             let height = cellPadding * 2 + photoHeight
             let frame = CGRect(x: xOffset[column], y: yOffset[column], width: columnWidth, height: height)
@@ -62,21 +69,36 @@ class PinterestLayout: UICollectionViewLayout {
         }
     }
     
+    override func layoutAttributesForSupplementaryView(ofKind elementKind: String, at indexPath: IndexPath) -> UICollectionViewLayoutAttributes? {
+        
+        guard let layoutAttributes = super.layoutAttributesForSupplementaryView(ofKind: elementKind, at: indexPath) else { return nil }
+        return layoutAttributes
+    }
+    
     override func layoutAttributesForElements(in rect: CGRect) -> [UICollectionViewLayoutAttributes]? {
+        super.layoutAttributesForElements(in: rect)
+        
         var visibleLayoutsAttributes: [UICollectionViewLayoutAttributes] = []
+
+        if let sectionHeaderAttributes = self.layoutAttributesForSupplementaryView(ofKind: UICollectionView.elementKindSectionHeader, at: IndexPath(row: 0, section: 0)) {
+            visibleLayoutsAttributes.append(sectionHeaderAttributes)
+        }
         
         for attributes in cache {
             if attributes.frame.intersects(rect) {
                 visibleLayoutsAttributes.append(attributes)
             }
         }
-        
+    
         return visibleLayoutsAttributes
     }
     
     override func layoutAttributesForItem(at indexPath: IndexPath) -> UICollectionViewLayoutAttributes? {
         return cache[indexPath.item]
     }
+
+    override func invalidateLayout() {
+        self.cache.removeAll()
+        super.invalidateLayout()
+    }
 }
-
-
--- BoysAndGirls/ViewControllers/BoysCollectionViewController.swift
+++ BoysAndGirls/ViewControllers/BoysCollectionViewController.swift
@@ -10,35 +10,44 @@ import UIKit
 
 class BoysCollectionViewController: UICollectionViewController {
     
+    var isPagging: Bool = false
+    var page: Int = 1
+    
     private struct Constants {
-        static let searchingString: String = "boy"
+        static let searchingString: String = "man face"
         static let cellID: String = "BoyCell"
         static let imageSize: String = "thumb"
+        static let headerViewHeight: CGFloat = 30.0
         
         static let leadingSectionIndent: CGFloat = 8.0
         static let trailingSectionIndent: CGFloat = 8.0
-        static let leadingTrailingSectionIndent: CGFloat = leadingSectionIndent + trailingSectionIndent
-        static let cellSpasing: CGFloat = 4.0
-        static let cellsInRow: CGFloat = 2
-        static let allCellsSpasing: CGFloat = cellSpasing * (cellsInRow - 1)
     }
     
     let photoModel = PhotoViewModel()
-    var isPagging: Bool = false
-    var page: Int = 1
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        self.setUpHeaderView()
+        self.setupObservers()
+        self.getData(byName: Constants.searchingString, onPage: page)
+    }
+    
+    override func viewWillLayoutSubviews() {
+        super.viewWillLayoutSubviews()
+        self.setLayoutDelegate()
+    }
+    
+    private func setLayoutDelegate() {
         if let layout = collectionView?.collectionViewLayout as? PinterestLayout {
             layout.delegate = self
         }
-        
-        self.collectionView.backgroundColor = .lightText
-        self.setupObservers()
-        photoModel.getPhotos(name: Constants.searchingString, onPage: page)
     }
     
+    private func setUpHeaderView() {
+        collectionView.register(HeaderView.self, forSupplementaryViewOfKind: UICollectionView.elementKindSectionHeader, withReuseIdentifier: HeaderView.reuseId)
+    }
+
     // waiting retrieve data
     private func setupObservers() {
         let notificationName = NSNotification.Name(rawValue: PhotoViewModel.notificationNameString)
@@ -50,6 +59,10 @@ class BoysCollectionViewController: UICollectionViewController {
             self.collectionView.reloadData()
         }
         self.isPagging = false
+    }
+    
+    private func getData(byName name: String, onPage page: Int) {
+        photoModel.getPhotos(name: name, onPage: page)
     }
     
     override func willRotate(to toInterfaceOrientation: UIInterfaceOrientation, duration: TimeInterval) {
@@ -65,13 +78,19 @@ class BoysCollectionViewController: UICollectionViewController {
 
 extension BoysCollectionViewController {
     
+    override func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        self.photoModel.photos.remove(at: indexPath.row)
+        let indexPathToDelete = IndexPath(row: indexPath.row, section: 0)
+        collectionView.deleteItems(at: [indexPathToDelete])
+    }
+    
     override func collectionView(_ collectionView: UICollectionView, willDisplay cell: UICollectionViewCell, forItemAt indexPath: IndexPath) {
+        
         // pagination
-        print(indexPath.row, photoModel.photos.count - 1)
         if indexPath.row == photoModel.photos.count - 1, !isPagging {
             isPagging = true
             page = page + 1
-            photoModel.getPhotos(name: Constants.searchingString, onPage: page)
+            self.getData(byName: Constants.searchingString, onPage: page)
         }
     }
 }
@@ -90,6 +109,20 @@ extension BoysCollectionViewController {
         cell.imageView.image = photoModel.photos[indexPath.row]
         return cell
     }
+    
+    override func collectionView(_ collectionView: UICollectionView, viewForSupplementaryElementOfKind kind: String, at indexPath: IndexPath) -> UICollectionReusableView {
+
+        switch kind {
+        case UICollectionView.elementKindSectionHeader:
+            guard let header = collectionView.dequeueReusableSupplementaryView(ofKind: UICollectionView.elementKindSectionHeader, withReuseIdentifier: HeaderView.reuseId, for: indexPath) as? HeaderView else { return UICollectionReusableView () }
+            header.labelView.text = "Boys"
+            return header
+            
+        default:
+            return UICollectionReusableView()
+        }
+    }
+    
 }
 
 // MARK: - UICollectionView delegate flow layout
@@ -103,6 +136,10 @@ extension BoysCollectionViewController: UICollectionViewDelegateFlowLayout {
     
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
         return UIEdgeInsets(top: 0, left: Constants.leadingSectionIndent, bottom: 0, right: Constants.trailingSectionIndent)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, referenceSizeForHeaderInSection section: Int) -> CGSize {
+        return CGSize(width: collectionView.frame.width, height: Constants.headerViewHeight)
     }
 }
 
